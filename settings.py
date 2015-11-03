d51681fff1cd921285ef99a54e1af88b43dc8ec4
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-11-03 20:50:53 +0000

    settings tweaks

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os.path
 here = os.path.dirname(__file__)
 
-BROKER_URL = 'sqla+sqlite:///' + os.path.join(here, 'celerydb.sqlite')
-CELERY_RESULT_BACKEND = 'db+sqlite:///'+ os.path.join(here, 'results.sqlite')
-CELERY_IGNORE_RESULT = False
+BROKER_URL                   = 'sqla+sqlite:///' + os.path.join(here, 'message_queue.sqlite')
+CELERY_RESULT_BACKEND        = 'db+sqlite:///'   + os.path.join(here, 'results.sqlite')
+CELERY_IGNORE_RESULT         = False
 CELERY_RESULT_ENGINE_OPTIONS = {'echo': True}
