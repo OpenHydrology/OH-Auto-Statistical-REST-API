4b4d2a4392e89d53ecb5ee951c5671ad9ecd60e6
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-11-04 20:59:22 +0000

    run autostatistical for real!

--- tasks.py
+++ tasks.py
@@ -1,16 +1,15 @@
 from core import celery, app
-import random
-import time
+import autostatistical
 
 
 @celery.task(bind=True)
 def do_analysis(self, catchment_file):
     """Background OH Auto Statistical analysis task."""
     message = ''
-    total = random.randint(10, 50)
-    for i in range(total):
-        self.update_state(state='PROGRESS',
-                          meta={'message': message})
-        time.sleep(1)
+    self.update_state(state='PROGRESS')
+
+    analysis = autostatistical.Analysis(catchment_file)
+    analysis.run()
+
     return {'message': message,
             'result': "OH Auto Statistical Report"}
