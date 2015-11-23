48509b3dfca85759e6ff891d17ac29804966e417
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-11-23 20:14:07 +0000

    remove appdirs settings

--- settings.py
+++ settings.py
@@ -1,7 +1,4 @@
 import os
-import appdirs
-here = os.path.dirname(__file__)
-data_folder = appdirs.user_data_dir('autostatistical', 'Open Hydrology')
 
 BROKER_URL                 = os.environ["CLOUDAMQP_URL"]
 CELERY_RESULT_BACKEND      = 'db+' + os.environ["DATABASE_URL"]
