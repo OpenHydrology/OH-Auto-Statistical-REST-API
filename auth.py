a7706a2c4a198c29742f5b260da1751e900bf9b3
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-12-03 17:55:52 +0000

    refactor token validation

--- auth.py
+++ auth.py
@@ -8,28 +8,42 @@ from werkzeug.exceptions import Unauthorized
 
 def requires_data_import_token(f):
     """
-    Decorator function for *simple token* authentication only.
+    Decorator function for *simple token string* authentication only.
 
     For server to server communication only. Token must not be passed to users or javascript clients.
     """
 
     @functools.wraps(f)
     def decorated(*args, **kwargs):
-        auth = flask.request.headers.get('Authorization', None)
-        if not auth:
-            raise Unauthorized('Authorization header is expected')
-        parts = auth.split()
-        if parts[0].lower() != 'bearer':
-            raise Unauthorized('Authorization header must start with Bearer')
-        elif len(parts) == 1:
-            raise Unauthorized('Token not found')
-        elif len(parts) > 2:
-            raise Unauthorized('Authorization header must be Bearer + \s + token')
-
-        token = parts[1]
+        token = auth_token(flask.request.headers)
         if token != core.app.flask_app.config['DATA_IMPORT_TOKEN']:  # just a straight comparison
             raise Unauthorized('Bearer token not valid')
 
         return f(*args, **kwargs)
 
     return decorated
+
+
+def auth_token(headers):
+    """
+    Return Bearer token from Authorization header.
+
+    Raises :class:`werkzeug.exceptions.Unauthorized` if no or invalid Authorization header provided.
+
+    :param headers: request headers
+    :type headers: dict
+    :return: Bearer token
+    :rtype: str
+    """
+    auth = headers.get('Authorization', None)
+    if not auth:
+        raise Unauthorized('Authorization header is expected')
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+        raise Unauthorized('Authorization header must start with Bearer')
+    elif len(parts) == 1:
+        raise Unauthorized('Token not found')
+    elif len(parts) > 2:
+        raise Unauthorized('Authorization header must be Bearer + \s + token')
+
+    return parts[1]
