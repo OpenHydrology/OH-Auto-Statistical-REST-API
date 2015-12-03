cf8de75e1a938c185d300b9a70f20a5bcf37bdd4
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-12-03 16:49:34 +0000

    use werkzeug exceptions

--- auth.py
+++ auth.py
@@ -3,12 +3,7 @@
 import functools
 import flask
 import core
-
-
-def authenticate(error):
-    resp = flask.jsonify(error)
-    resp.status_code = 401
-    return resp
+from werkzeug.exceptions import Unauthorized
 
 
 def requires_data_import_token(f):
@@ -22,32 +17,18 @@ def requires_data_import_token(f):
     def decorated(*args, **kwargs):
         auth = flask.request.headers.get('Authorization', None)
         if not auth:
-            return authenticate({
-                'code': 'authorization_header_missing',
-                'description': 'Authorization header is expected'
-            })
+            raise Unauthorized('Authorization header is expected')
         parts = auth.split()
         if parts[0].lower() != 'bearer':
-            return authenticate({
-                'code': 'invalid_header',
-                'description': 'Authorization header must start with Bearer'})
+            raise Unauthorized('Authorization header must start with Bearer')
         elif len(parts) == 1:
-            return authenticate({
-                'code': 'invalid_header',
-                'description': 'Token not found'
-            })
+            raise Unauthorized('Token not found')
         elif len(parts) > 2:
-            return authenticate({
-                'code': 'invalid_header',
-                'description': 'Authorization header must be Bearer + \s + token'
-            })
+            raise Unauthorized('Authorization header must be Bearer + \s + token')
 
         token = parts[1]
         if token != core.app.flask_app.config['DATA_IMPORT_TOKEN']:  # just a straight comparison
-            return authenticate({
-                'code': 'invalid_header',
-                'description': 'Bearer token not valid'
-            })
+            raise Unauthorized('Bearer token not valid')
 
         return f(*args, **kwargs)
 
