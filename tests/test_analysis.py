13e2a82ce72c4456c609dd68ee647a703eb311dc
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-12-09 21:21:38 +0000

    patch entire TestCase

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -5,9 +5,10 @@ import tasks
 from unittest import mock
 
 
+@mock.patch.object(tasks.do_analysis_from_id, 'run', autospec=True)
+@mock.patch.object(tasks.do_analysis, 'run', autospec=True)
 class AnalysisTestCase(unittest.TestCase):
     API_URL = '/api/v0'
-
 
     @classmethod
     def setUpClass(cls):
@@ -16,28 +17,27 @@ class AnalysisTestCase(unittest.TestCase):
     def setUp(self):
         self.test_client = core.app.flask_app.test_client()
 
-    def test_no_data(self):
+    def test_no_data(self, do_analysis, do_analysis_from_id):
         resp = self.test_client.post(self.API_URL + '/analyses/')
         data = flask.json.loads(resp.get_data())
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(data['message'], "Catchment file (.cd3 or .xml) required.")
 
-    @mock.patch.object(tasks.do_analysis_from_id, 'run', autospec=True)
-    def test_catchment_id_only(self, do_analysis_from_id_mock):
+    def test_catchment_id_only(self, do_analysis, do_analysis_from_id):
         form_data = {'nrfa-id': 3002}
         resp = self.test_client.post(self.API_URL + '/analyses/', data=form_data)
         self.assertEqual(resp.status_code, 202)
         self.assertIn(self.API_URL + '/analysis-tasks/', resp.headers['Location'])
-        do_analysis_from_id_mock.assert_called_with(3002)
+        do_analysis_from_id.assert_called_with(3002)
 
-    def test_non_cd3_file(self):
+    def test_non_cd3_file(self, do_analysis, do_analysis_from_id):
         form_data = {'file': (open('tests/data/8002.CD3', 'rb'), '8002.bla')}
         resp = self.test_client.post(self.API_URL + '/analyses/', data=form_data)
         data = flask.json.loads(resp.get_data())
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(data['message'], "Catchment file (.cd3 or .xml) required.")
 
-    def test_cd3_plus_non_am_file(self):
+    def test_cd3_plus_non_am_file(self, do_analysis, do_analysis_from_id):
         form_data = {'file1': (open('tests/data/8002.CD3', 'rb'), '8002.CD3'),
                      'file2': (open('tests/data/8002.AM', 'rb'), '8002.bla')}
         resp = self.test_client.post(self.API_URL + '/analyses/', data=form_data)
@@ -45,7 +45,7 @@ class AnalysisTestCase(unittest.TestCase):
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(data['message'], "Second file must be AMAX (.am) file.")
 
-    def test_too_many_files(self):
+    def test_too_many_files(self, do_analysis, do_analysis_from_id):
         form_data = {'file1': (open('tests/data/8002.CD3', 'rb'), '8002.CD3'),
                      'file2': (open('tests/data/8002.AM', 'rb'), '8002.am'),
                      'file3': (open('tests/data/8002.AM', 'rb'), '8002.bla')}
@@ -54,18 +54,16 @@ class AnalysisTestCase(unittest.TestCase):
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(data['message'], "Too many files supplied.")
 
-    @mock.patch.object(tasks.do_analysis, 'run', autospec=True)
-    def test_cd3_file_only(self, do_analysis_mock):
+    def test_cd3_file_only(self, do_analysis, do_analysis_from_id):
         with open('tests/data/8002.CD3', 'r') as cd3_f:
             cd3_content = cd3_f.read()
         form_data = {'file': (open('tests/data/8002.CD3', 'rb'), '8002.CD3')}
         resp = self.test_client.post(self.API_URL + '/analyses/', data=form_data)
         self.assertEqual(resp.status_code, 202)
         self.assertIn(self.API_URL + '/analysis-tasks/', resp.headers['Location'])
-        do_analysis_mock.assert_called_with(cd3_content, amax_str=None)
+        do_analysis.assert_called_with(cd3_content, amax_str=None)
 
-    @mock.patch.object(tasks.do_analysis, 'run', autospec=True)
-    def test_cd3_and_am_file(self, do_analysis_mock):
+    def test_cd3_and_am_file(self, do_analysis, do_analysis_from_id):
         with open('tests/data/8002.CD3', 'r') as cd3_f:
             cd3_content = cd3_f.read()
         with open('tests/data/8002.AM', 'r') as am_f:
@@ -75,9 +73,9 @@ class AnalysisTestCase(unittest.TestCase):
         resp = self.test_client.post(self.API_URL + '/analyses/', data=form_data)
         self.assertEqual(resp.status_code, 202)
         self.assertIn(self.API_URL + '/analysis-tasks/', resp.headers['Location'])
-        do_analysis_mock.assert_called_with(cd3_content, amax_str=am_content)
+        do_analysis.assert_called_with(cd3_content, amax_str=am_content)
 
-    def test_get_non_existent_analysis(self):
+    def test_get_non_existent_analysis(self, do_analysis, do_analysis_from_id):
         resp = self.test_client.get(self.API_URL + '/analyses/bla')
         data = flask.json.loads(resp.get_data())
         self.assertEqual(resp.status_code, 404)
