c49332834fa5492d4e035a3cc6a8da4b0e39eaed
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-12-21 19:08:43 +0000

    use fake client_id and secret for testing

--- tests/test_dataimport.py
+++ tests/test_dataimport.py
@@ -3,8 +3,8 @@ import core
 import flask
 import tasks
 from unittest import mock
-import os
-
+import jwt
+import base64
 
 @mock.patch.object(tasks.import_data, 'run', autospec=True)
 class DataImportTestCase(unittest.TestCase):
@@ -13,6 +13,15 @@ class DataImportTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         core.celery.conf['CELERY_ALWAYS_EAGER'] = True
+        payload = {
+            'sub': 0,
+            'aud': core.app.flask_app.config['AUTH_CLIENT_ID'],
+            'roles': ['importer']
+        }
+        cls.auth_token = jwt.encode(
+            payload,
+            base64.b64decode(core.app.flask_app.config['AUTH_CLIENT_SECRET'].replace("_", "/").replace("-", "+"))
+        ).decode()
 
     def setUp(self):
         self.test_client = core.app.flask_app.test_client()
@@ -57,7 +66,7 @@ class DataImportTestCase(unittest.TestCase):
         self.assertFalse(import_data.called)
 
     def test_plain_text_body(self, import_data):
-        headers = {'Authorization': 'Bearer ' + os.environ['TEST_IMPORT_TOKEN']}
+        headers = {'Authorization': 'Bearer ' + self.auth_token}
         body = "bla"
         resp = self.test_client.post(self.API_URL + '/data-imports/', headers=headers, data=body)
         data = flask.json.loads(resp.get_data())
@@ -66,7 +75,7 @@ class DataImportTestCase(unittest.TestCase):
         self.assertFalse(import_data.called)
 
     def test_json_body_no_url(self, import_data):
-        headers = {'Authorization': 'Bearer ' + os.environ['TEST_IMPORT_TOKEN']}
+        headers = {'Authorization': 'Bearer ' + self.auth_token}
         body = flask.json.dumps({'bla': 'bla'})
         resp = self.test_client.post(self.API_URL + '/data-imports/', data=body, headers=headers,
                                      content_type='application/json')
@@ -76,7 +85,7 @@ class DataImportTestCase(unittest.TestCase):
         self.assertFalse(import_data.called)
 
     def test_non_zip_url(self, import_data):
-        headers = {'Authorization': 'Bearer ' + os.environ['TEST_IMPORT_TOKEN']}
+        headers = {'Authorization': 'Bearer ' + self.auth_token}
         body = flask.json.dumps({'url': 'https://bla.com/file.tar.gz'})
         resp = self.test_client.post(self.API_URL + '/data-imports/', data=body, headers=headers,
                                      content_type='application/json')
@@ -89,7 +98,7 @@ class DataImportTestCase(unittest.TestCase):
 
     def test_non_existent_zip_url(self, import_data):
         # The REST API does not check if url exists or not, so this should return 202 (Accepted)
-        headers = {'Authorization': 'Bearer ' + os.environ['TEST_IMPORT_TOKEN']}
+        headers = {'Authorization': 'Bearer ' + self.auth_token}
         body = flask.json.dumps({'url': 'https://bla.com/file.zip'})
         resp = self.test_client.post(self.API_URL + '/data-imports/', data=body, headers=headers,
                                      content_type='application/json')
